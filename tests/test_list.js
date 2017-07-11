5d5c576d677ee67bc4c23e8f59d51c12ae1a3df3
Blake Loring <bloring@cloudflare.com>
2017-07-11 11:48:47 -0700

    Add the plus tests to the test list

--- tests/test_list.js
+++ tests/test_list.js
@@ -66,6 +66,9 @@ function buildTestList() {
     buildTest('regex/test/star/star_online.js', 11, 10);
     buildTest('regex/test/star/lazy.js', 7, 7);
 
+    buildTest('regex/test/plus/plus.js', 12, 11);
+    buildTest('regex/test/plus/lazy.js', 12, 11);
+
     //buildTest('regex/test/anchors/left.js', 5, 5); //TODO: This has more tests than it should
     //buildTest('regex/test/anchors/none.js', 6, 6); //TODO: This has more tests than it should
 
