da74aa945758a718241bf32544543f8482bab60a
Blake Loring <bloring@cloudflare.com>
2017-07-11 11:28:10 -0700

    Add unit test for star

--- tests/regex/test/star/lazy.js
+++ tests/regex/test/star/lazy.js
@@ -7,6 +7,7 @@ if (/^he*?llo_world*?$/.test(x)) {
 	if (x == 'heello_worlddddd') throw 'Reachable';
 	if (x == 'hllo_worl') throw 'Reachable';
 	if (x == '') throw 'Unreachable';
+	throw 'Reachable';
 }
 
 if (/(hello)*?(world)*?/.test(x)) {
@@ -15,4 +16,6 @@ if (/(hello)*?(world)*?/.test(x)) {
 	if (x == 'd') throw 'Unreachable';
 	if (x == 'hellohelloworl') throw 'Unreachable';
 	if (x == 'worldworldworld') throw 'Reachable';
+
+	throw 'Reachable';
 }
