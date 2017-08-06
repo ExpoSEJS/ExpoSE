6adc812d56cc064ac16ce25d0ecfe2aadea9f357
Blake Loring <blake_l@parsed.uk>
2017-08-06 17:29:31 +0100

    Test updates

--- tests/regex/test/atoms/non_alpha.js
+++ tests/regex/test/atoms/non_alpha.js
@@ -1,8 +1,8 @@
 var x = symbolic X initial '';
-var isIn = /void helloWorld\(\) \{\{\}\} DOGDOGDOG console.log\(HELLO WORLD\) HOW ARE YOU/.test(x);
+var isIn = /^void helloWorld\(\) \{\{\}\} DOGDOGDOG console.log\(HELLO WORLD\) HOW ARE YOU$/.test(x);
 
 if (isIn) {
 	throw 'Reachable';
 } else {
-	throw 'Unreachable';
+	throw 'Reachable';
 }
