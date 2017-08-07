3dc4fbc80a2f74c17e6f1036963294f2492d7705
Blake Loring <bloring@cloudflare.com>
2017-08-07 14:04:15 +0100

    Tweak negative range

--- tests/regex/test/escaping/negative_ranges.js
+++ tests/regex/test/escaping/negative_ranges.js
@@ -1,22 +1,27 @@
 var x = symbolic X initial '';
 
 if (/^[^abcd]$/.test(x)) {
-	if (x == 'c' || x == 'a') throw 'Unreachable';
-	if (x == '') throw 'Unreachable';
-	if (x == 'e' || x == 'p') throw 'Reachable';
+	if (x == 'a') throw 'Unreachable';
+	if (x == 'b') throw 'Unreachable';
+	if (x == 'c') throw 'Unreachable';
+	if (x == 'd') throw 'Unreachable';
 	throw 'Reachable';
 }
 
-if (/^[^a-z]*$/.test(x)) {
-	if (x == 'abcdef' || x == '123abc') throw 'Unreachable';
-	if (x == '') throw 'Reachable';
-	if (x == '12345' || x == '1$"£"!%^32131') throw 'Reachable';
+if (/^[^a-z]$/.test(x)) {
+	
+	if (/^[a-z]$/.test(x)) {
+		throw 'Unreachable';
+	}
+
 	throw 'Reachable';
 }
 
 if (/^[^0-9]+$/.test(x)) {
-	if (x == '12345' || x == '12ab34') throw 'Unreachable';
-	if (x == 'abc') throw 'Reachable';
-	if (x == '') throw 'Unreachable';
+	
+	if (/^[0-9]$/.test(x)) {
+		throw 'Unreachable';
+	}
+
 	throw 'Reachable';
 }
