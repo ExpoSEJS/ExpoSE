998f4bd51d1ce6ddb138b19d8469dd4a7b491e5c
Blake Loring <blake_l@parsed.uk>
2017-08-09 22:02:54 +0100

    Anchor related.js

--- tests/regex/exec/captures/related.js
+++ tests/regex/exec/captures/related.js
@@ -1,9 +1,7 @@
 //In this test a string constraint is used to force a relation between two captured strings
 
 var x = symbolic X initial '';
-var b = /(.+)q(.+)/.exec(x);
-
-assume x.length < 20;
+var b = /^(.+)q(.+)$/.exec(x);
 
 if (b) {
 	if (b[1] == b[2]) throw 'Reachable';
