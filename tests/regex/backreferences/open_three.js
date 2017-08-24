9aa4b858119997ec3b39f88dd49aec0db53b91e8
Blake Loring <bloring@cloudflare.com>
2017-08-24 11:22:04 +0100

    Add open_three

---
+++ tests/regex/backreferences/open_three.js
@@ -0,0 +1,36 @@
+//Test modeling of looped references to open capture groups (Local captures)
+
+var x = symbolic X initial '';
+
+if (/^(([a-z])\2)+(([1-9])\4)+$/.test(x)) {
+
+	if (x == '11') {
+		throw 'Unreachable';
+	}
+
+	if (x == 'ab') {
+		throw 'Unreachable';
+	}
+
+	if (x == 'aa') {
+		throw 'Unreachable';
+	}
+
+	if (x == 'zz') {
+		throw 'Unreachable';
+	}
+
+	if (x == 'aa23') {
+		throw 'Unreachable';
+	}
+
+	if (x == 'aa11') {
+		throw 'Reachable';
+	}
+
+	if (x == 'aaaa11') {
+		throw 'Reachable';
+	}
+
+	throw 'Reachable';
+}
