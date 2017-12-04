3b8580ff0a32cbdafb99313141c096c308574f4b
Blake Loring <blake_l@parsed.uk>
2017-12-04 11:42:49 +0000

    Test slice seperately

--- tests/strings/negative_slice.js
+++ tests/strings/negative_slice.js
@@ -2,34 +2,34 @@ var x = symbolic X initial "hello";
 
 assume x.length == 5;
 
-if (x.substr(1, 1) == "w") {
+if (x.slice(1, 1) == "w") {
 	throw 'Reachable';
 }
 
-if (x.substr(1) == "what") {
+if (x.slice(1) == "what") {
 	throw 'Unreachable';
 }
 
-if (x.substr(1) == "phat") {
+if (x.slice(1) == "phat") {
 	throw 'Reachable 2';
 }
 
-if (x.substr(-1) == "w") {
+if (x.slice(-1) == "w") {
 	throw 'Reachable 3';
 }
 
-if (x.substr(-5, 3) == "por") {
+if (x.slice(-5, 3) == "por") {
 	throw 'Reachable 4';
 }
 
-if (x.substr(-3, 8) == "por") {
+if (x.slice(-3, 8) == "por") {
 	throw 'Reachable 5';
 }
 
-if (x.substr(-5, 4) == "por") {
+if (x.slice(-5, 4) == "por") {
 	throw 'Unreachable';
 }
 
-if (x.substr(-1) == "what") {
+if (x.slice(-1) == "what") {
 	throw 'Unreachable';
 }
