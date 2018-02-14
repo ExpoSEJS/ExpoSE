5651ed32a09858b609622020cd128e296860baad
Blake Loring <blake@parsed.uk>
2018-02-14 14:08:55 +0000

    Fix a formatting issue

--- Analyser/src/SymbolicState.js
+++ Analyser/src/SymbolicState.js
@@ -248,7 +248,7 @@ class SymbolicState {
         }
 
         function makeid(count) {
-        var text = "";
+            var text = "";
             var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";    
             
             for (var i = 0; i < count; i++) {
