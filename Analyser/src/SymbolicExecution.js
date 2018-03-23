a86150b9ceea1a7312c6cd06fe76fcd558657aae
Blake Loring <blake@parsed.uk>
2018-03-23 18:12:45 +0000

    Use is_null to block valueOf on binaryPre

--- Analyser/src/SymbolicExecution.js
+++ Analyser/src/SymbolicExecution.js
@@ -308,14 +308,14 @@ class SymbolicExecution {
             const left_c  = this.state.getConcrete(left),
                   right_c = this.state.getConcrete(right);
 
-            //TODO: Work out how to check that boxed values are the same type
-            const is_same_type = typeof(left_c) === typeof(right_c) || (left_c.valueOf && right_c.valueOf && left_c.valueOf() == right_c.valueOf());
-            
             //We also consider boxed primatives to be primative
             const is_primative = typeof(left_c) != 'object' || (left_c instanceof Number || left_c instanceof String || left_c instanceof Boolean);
             const is_null = left_c === undefined || right_c === undefined || left_c === null || right_c === null;  
             const is_real = typeof(left_c) == "number" ? (Number.isFinite(left_c) && Number.isFinite(right_c)) : true;
  
+            //TODO: Work out how to check that boxed values are the same type
+            const is_same_type = typeof(left_c) === typeof(right_c) || (!is_null && left_c.valueOf() == right_c.valueOf());
+            
             if (!is_same_type || !is_primative || is_null || !is_real) {
                 Log.log("Concretizing binary " + op + " on operands of differing types. Type coercion not yet implemented symbolically. (" + ObjectHelper.asString(left_c) + ", " + ObjectHelper.asString(right_c) + ') (' + typeof left_c + ', ' + typeof right_c + ')');
                 left = left_c;
