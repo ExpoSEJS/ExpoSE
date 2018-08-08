1a3aeafe8f22d5e80276a7e0caaae99a274f2641
Blake <blake@parsed.uk>
2018-08-08 10:24:26 +0100

    Remove external electron link as we're rethinking that for now

--- Analyser/src/External.js
+++ Analyser/src/External.js
@@ -2,21 +2,6 @@
 
 "use strict";
 
-/**
- * This is a function that detects whether we are using Electron and 
- * if so does a remote call through IPC rather than a direct require.
- */
-
- const ELECTRON_PATH_MAGIC = '/electron/';
-
 export default function (library) {
-	let lrequire = require;
-
-	if (process && process.execPath && process.execPath.indexOf(ELECTRON_PATH_MAGIC) != -1) {
-		lrequire = require('electron').remote.require;
-	}
-
-	const result = lrequire(library);
-
-	return result.default ? result.default : result;
+	return require(library);
 }
