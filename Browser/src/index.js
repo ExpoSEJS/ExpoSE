0dd5bbd4b9c001838922d6f0efc830e030066a42
Blake Loring <blake@parsed.uk>
2018-09-08 08:26:22 +0100

    Working browser support!

--- Browser/src/index.js
+++ Browser/src/index.js
@@ -1,12 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
-
 console.log('Args: ' + JSON.stringify(process.argv));
-
 app.commandLine.appendSwitch('proxy-server', '127.0.0.1:8080');
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -22,11 +16,12 @@ const createWindow = () => {
 
   mainWindow.webContents.session.setProxy({proxyRules:"http://localhost:8080"}, function () {
     mainWindow.loadURL(process.argv[process.argv.length - 2]);
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
   });
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
+    console.log('Main window closed');
     mainWindow = null;
   });
 };
