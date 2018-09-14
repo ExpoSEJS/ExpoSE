29d30b1fc872d50d55a346109f29e978f6f7769c
Blake Loring <blake@parsed.uk>
2018-09-14 16:25:36 +0100

    Relicensing

--- tests/arrays/pop_conc.js
+++ tests/arrays/pop_conc.js
@@ -1,3 +1,5 @@
+/* Copyright (c) Royal Holloway, University of London | Contact Blake Loring (blake@parsed.uk), Duncan Mitchell (Duncan.Mitchell.2015@rhul.ac.uk), or Johannes Kinder (johannes.kinder@rhul.ac.uk) for details or support | LICENSE.md for license details */
+
 var x = [1, 2, 3];
 
 console.log('X is ' + x + ' and JSONd ' + JSON.stringify(x));
@@ -13,4 +15,3 @@ if (x.pop() != 3) {
 if (x.length != 2) {
     throw 'Unreachable 3';
 }
-
--- tests/regex/match/greed/related.js
+++ tests/regex/match/greed/related.js
@@ -1,3 +1,5 @@
+/* Copyright (c) Royal Holloway, University of London | Contact Blake Loring (blake@parsed.uk), Duncan Mitchell (Duncan.Mitchell.2015@rhul.ac.uk), or Johannes Kinder (johannes.kinder@rhul.ac.uk) for details or support | LICENSE.md for license details */
+
 //In this test a string constraint is used to force a relation between two captured strings
 //The Regex in question though places the implicit constraint that C2 can only ever be one character wide though
 var S$ = require('S$');
