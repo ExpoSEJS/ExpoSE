3687aba767358af9811524e1e496856cc5209e4f
Blake Loring <blake@parsed.uk>
2018-09-18 23:51:19 +0100

    Fix bug with External

--- Analyser/src/External.js
+++ Analyser/src/External.js
@@ -1,12 +1,13 @@
 /* Copyright (c) Royal Holloway, University of London | Contact Blake Loring (blake@parsed.uk), Duncan Mitchell (Duncan.Mitchell.2015@rhul.ac.uk), or Johannes Kinder (johannes.kinder@rhul.ac.uk) for details or support | LICENSE.md for license details */
 
-
+//Cache electron so require doesn't get rewritten
+const electron = require("electron");
 
 export default {
 
     load: function (library) {
         if (this.is_external()) {
-            return require("electron").remote.require(library);
+            return electron.remote.require(library);
         } else {
             return require(library);
         }
