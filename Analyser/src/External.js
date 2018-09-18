96877378c05a53e6d151163620f649adca5acae8
Blake Loring <blake@parsed.uk>
2018-09-19 00:11:31 +0100

    Fix broken External with require-write fix

--- Analyser/src/External.js
+++ Analyser/src/External.js
@@ -1,20 +1,18 @@
 /* Copyright (c) Royal Holloway, University of London | Contact Blake Loring (blake@parsed.uk), Duncan Mitchell (Duncan.Mitchell.2015@rhul.ac.uk), or Johannes Kinder (johannes.kinder@rhul.ac.uk) for details or support | LICENSE.md for license details */
 
+function is_external() {
+    return typeof window !== "undefined";
+}
+
 //Cache electron so require doesn't get rewritten
-const electron = require("electron");
+const require = is_external() ? require("electron").remote.require : require;
 
 export default {
 
     load: function (library) {
-        if (this.is_external()) {
-            return electron.remote.require(library);
-        } else {
-            return require(library);
-        }
+        return require(library);
     },
 
-    is_external: function() {
-        return typeof window !== "undefined";
-    }
+    is_external: is_external
 
 };
