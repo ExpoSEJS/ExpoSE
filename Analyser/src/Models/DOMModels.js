4cd425a73db7a7dfb8c0303229804a00bf8a1578
Blake Loring <blake@parsed.uk>
2019-02-04 12:44:47 +0000

    Check window for Element state

--- Analyser/src/Models/DOMModels.js
+++ Analyser/src/Models/DOMModels.js
@@ -1,4 +1,6 @@
 export default function(state, ctx, models, helper) {
-  models.add(Element.prototype.getAttribute, helper.NoOp);
-  models.add(Element.prototype.setAttribute, helper.NoOp);
+  if (typeof window !== "undefined") {
+    models.add(Element.prototype.getAttribute, helper.NoOp);
+    models.add(Element.prototype.setAttribute, helper.NoOp);
+  }
 }
