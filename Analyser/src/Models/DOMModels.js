3d065ce642d4b9ace6baecf5f58426393b50d4fe
Blake Loring <blake@parsed.uk>
2019-02-25 08:53:11 +0000

    EncodeURI prototype

--- Analyser/src/Models/DOMModels.js
+++ Analyser/src/Models/DOMModels.js
@@ -2,8 +2,12 @@ export default function(state, ctx, models, helper) {
   if (typeof window !== "undefined") {
     models.add(Element.prototype.getAttribute, helper.NoOp(Element.prototype.getAttribute));
     models.add(Element.prototype.setAttribute, helper.NoOp(Element.prototype.setAttribute));
-    models.add(encodeURI, function(base, args) {
-      return args[0];
-    });
   }
+
+	models.add(encodeURI, function(base, args) {
+		const replace = models.get(String.prototype.replace);
+		return replace.call(/[^A-Za-z0-9;,/?:@&=+$-_.!~*'()#]/g, function(match) {
+			return state.binary('+', '%', match.charCodeAt(0));
+		});
+	});
 }
