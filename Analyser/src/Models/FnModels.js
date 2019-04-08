0239d05fe031f8c0a8899339c265e9e95019060a
Blake Loring <blake@parsed.uk>
2019-04-08 14:51:52 +0100

    Add a shallow concretizing hasOwnProperty prototyp

--- Analyser/src/Models/FnModels.js
+++ Analyser/src/Models/FnModels.js
@@ -6,7 +6,21 @@ export default function(state, ctx, model, helpers) {
 	model.add(Function.prototype.apply, ConcretizeIfNative(Function.prototype.apply));
 	model.add(Function.prototype.call, ConcretizeIfNative(Function.prototype.call));
 	model.add(Function.prototype.bind, ConcretizeIfNative(Function.prototype.bind));
+  model.add(Function.prototype.hasOwnProperty, function(base, args) {
+
+    for (let i = 0; i < args.length; i++) {
+      args[i] = state.getConcrete(args[i]);
+    }
+
+    Function.prototype.hasOwnProperty.apply(state.getConcrete(base), args); 
+  });
+ 
   model.add(console.log, function(base, args) {
+
+    for (let i = 0; i < args.length; i++) {
+      args[i] = state.getConcrete(args[i]);
+    }
+
 		console.log.apply(base, args);
 	});
 }
