ef9d911acbfe418dec9567be1084fd56e75aa9f4
Blake Loring <blake@parsed.uk>
2019-04-09 09:58:43 +0100

    Add basic model for keys

--- Analyser/src/Models/FnModels.js
+++ Analyser/src/Models/FnModels.js
@@ -6,13 +6,23 @@ export default function(state, ctx, model, helpers) {
 	model.add(Function.prototype.apply, ConcretizeIfNative(Function.prototype.apply));
 	model.add(Function.prototype.call, ConcretizeIfNative(Function.prototype.call));
 	model.add(Function.prototype.bind, ConcretizeIfNative(Function.prototype.bind));
-  model.add(Function.prototype.hasOwnProperty, function(base, args) {
+
+  model.add(Object.prototype.hasOwnProperty, function(base, args) {
 
     for (let i = 0; i < args.length; i++) {
       args[i] = state.getConcrete(args[i]);
     }
 
     Function.prototype.hasOwnProperty.apply(state.getConcrete(base), args); 
+  });
+
+  model.add(Object.prototype.keys, function(base, args) {
+
+    for (let i = 0; i < args.length; i++) {
+      args[i] = state.getConcrete(args[i]);
+    }
+
+    return Object.prototype.keys.apply(this.getConcrete(base), args);
   });
  
   model.add(console.log, function(base, args) {
