bd4f8fb5db26748164188746ce426d4fd7788289
Blake Loring <blake@parsed.uk>
2019-07-03 09:10:21 -0600

    Binary & Unary

--- Analyser/src/SymbolicExecution.js
+++ Analyser/src/SymbolicExecution.js
@@ -251,46 +251,46 @@ class SymbolicExecution {
 
 		Log.logHigh(`Get field ${ObjectHelper.asString(base)}[${ObjectHelper.asString(offset)}] at ${this._location(iid)}`);
 
-		let result = undefined;
+    let result = undefined;
 
 		if (base instanceof SymbolicObject) {
-			//If dealing with a SymbolicObject then concretize the offset and defer to SymbolicObject.getField
+      //If dealing with a SymbolicObject then concretize the offset and defer to SymbolicObject.getField
 			Log.logMid("Potential loss of precision, cocretize offset on SymbolicObject field lookups");
 			result = base.getField(this.state, this.state.getConcrete(offset));
 		} else if (!this.state.isSymbolic(base) && this.state.isSymbolic(offset) && typeof this.state.getConcrete(offset) == "string") {
-			//If we are evaluating a symbolic string offset on a concrete base then enumerate all fields
-			//Then return the concrete lookup
+      //If we are evaluating a symbolic string offset on a concrete base then enumerate all fields
+      //Then return the concrete lookup
 			this._getFieldSymbolicOffset(base, offset);
-			result = base[this.state.getConcrete(offset)];
+      result = base[this.state.getConcrete(offset)];
 		} else if (!this.state.isSymbolic(base) &&
              this.state.isSymbolic(offset) &&
              this.state.getConcrete(base) instanceof Array &&
              typeof this.state.getConcrete(offset) == "number") {
-			//If the array is a symbolic int and the base is a concrete array then enumerate all the indices
+      //If the array is a symbolic int and the base is a concrete array then enumerate all the indices
 			for (let i = 0; i < this.state.getConcrete(base).length; i++) {
 				this.state.assertEqual(i, offset); 
 			}
 
-			result = base[this.state.getConcrete(offset)];
+      result = base[this.state.getConcrete(offset)];
 		} else {
-			//Otherwise defer to symbolicField
-			const result_s = this.state.isSymbolic(base) ? this.state.symbolicField(this.state.getConcrete(base), this.state.asSymbolic(base), this.state.getConcrete(offset), this.state.asSymbolic(offset)) : undefined;
-			const result_c = this.state.getConcrete(base)[this.state.getConcrete(offset)];
-			result = result_s ? new ConcolicValue(result_c, result_s) : result_c;
-		}
+      //Otherwise defer to symbolicField
+      const result_s = this.state.isSymbolic(base) ? this.state.symbolicField(this.state.getConcrete(base), this.state.asSymbolic(base), this.state.getConcrete(offset), this.state.asSymbolic(offset)) : undefined;
+      const result_c = this.state.getConcrete(base)[this.state.getConcrete(offset)];
+      result = result_s ? new ConcolicValue(result_c, result_s) : result_c;
+    }
 
     
-		if (this.state.isWrapped(base)) {
-			base.reduceAndDiscard(base, annotation => {
-				let [discard, result_a] = annotation.getField(base, offset, result);
-				result = result_a;
-				return discard;
-			});
-		}
+    if (this.state.isWrapped(base)) {
+      base.reduceAndDiscard(base, annotation => {
+        let [discard, result_a] = annotation.getField(base, offset, result);
+        result = result_a;
+        return discard;
+      });
+    }
 
-		return {
-			result: result
-		};
+    return {
+      result: result
+    }
 	}
 
 	putFieldPre(iid, base, offset, val, _isComputed, _isOpAssign) {
@@ -303,13 +303,13 @@ class SymbolicExecution {
 			val = this.state.getConcrete(val);
 		}
 
-		if (this.state.isWrapped(base)) {
-			base.reduceAndDiscard(annotation => {
-				let [discard, result_a] = annotation.putField(base, offset, val);
-				val = result_a;
-				return discard;
-			});
-		}
+    if (this.state.isWrapped(base)) {
+      base.reduceAndDiscard(annotation => {
+        let [discard, result_a] = annotation.putField(base, offset, val);
+        val = result_a;
+        return discard;
+      });
+    }
 
 		return {
 			base: base,
@@ -483,34 +483,30 @@ class SymbolicExecution {
 		};
 	}
 
-	binary(iid, op, left, right, result_c, _isOpAssign, _isSwitchCaseComparison, _isComputed) {
+	binary(iid, op, left, right, result, _isOpAssign, _isSwitchCaseComparison, _isComputed) {
 		this.state.coverage.touch(iid);
 
-		Log.logHigh("Op " + op + " left " + ObjectHelper.asString(left) + " right " + ObjectHelper.asString(right) + " result_c " + ObjectHelper.asString(result_c) + " at " + this._location(iid));
-
-		let result;
+		Log.logHigh("Op " + op + " left " + ObjectHelper.asString(left) + " right " + ObjectHelper.asString(right) + " result " + ObjectHelper.asString(result) + " at " + this._location(iid));
 
 		if (this.state.isSymbolic(left) || this.state.isSymbolic(right)) {
 			result = this.state.binary(op, left, right);
 		} else if (this.state.isWrapped(left) || this.state.isWrapped(right)) {
-			result = SymbolicHelper.evalBinary(op, this.state.getConcrete(left), this.state.getConcrete(right)); 
-		} else {
-			result = result_c;
-		}
+      result = SymbolicHelper.evalBinary(op, this.state.getConcrete(left), this.state.getConcrete(right)); 
+    }
 
-		let atBinary = annotation => {
-			let [discard, result_a] = annotation.binary(op, left, right, true, result);
-			result = result_a;
-			return discard;
-		};
+    let atBinary = annotation => {
+      let [discard, result_a] = annotation.binary(op, left, right, true, result);
+      result = result_a;
+      return discard;
+    };
 
-		if (this.state.isWrapped(left)) {
-			left.reduceAndDiscard(atBinary);
-		}
+    if (this.state.isWrapped(left)) {
+      left.reduceAndDiscard(atBinary);
+    }
 
-		if (this.state.isWrapped(right)) {
-			right.reduceAndDiscard(atBinary);
-		}
+    if (this.state.isWrapped(right)) {
+      right.reduceAndDiscard(atBinary);
+    }
 
 		return {
 			result: result
@@ -527,13 +523,27 @@ class SymbolicExecution {
 		};
 	}
 
-	unary(iid, op, left, result_c) {
+	unary(iid, op, left, result) {
 		this.state.coverage.touch(iid);
 
-		Log.logHigh("Unary " + op + " left " + ObjectHelper.asString(left) + " result " + ObjectHelper.asString(result_c)); 
+		Log.logHigh("Unary " + op + " left " + ObjectHelper.asString(left) + " result " + ObjectHelper.asString(result)); 
+
+    if (this.state.isSymbolic(left)) {
+      result = this.state.unary(op, left);
+    } else {
+      result = SymbolicHelper.evalUnary(op, left);
+    }
+
+    if (this.state.isWrapped(left)) {
+      left.reduceAndDiscard(annotation => {
+        let [discard, result_a] = annotation.unary(op, left);
+        result = result_a;
+        return discard;
+      });
+    }
 
 		return {
-			result: this.state.unary(op, left)
+			result: result
 		};
 	}
 
