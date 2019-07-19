5469ed5ec9ce11e82e7bfde39bcce484d2533dcc
Duncan Mitchell <duncan.e.i.mitchell@gmail.com>
2019-07-19 09:49:05 +0100

    SecAnn + bool test

--- tests/annotations/ann.js
+++ tests/annotations/ann.js
@@ -9,11 +9,13 @@ var B = S$.Annotations.Annotation.create('B', []);
 console.log('C');
 var C = S$.Annotations.Annotation.create('C', []);
 
+console.log('first annotate starting');
 var a = S$.annotate(true, new A([]));
-console.log('HI');
+console.log('first annotate done');
 
+console.log('second annotate starting');
 a = S$.annotate(a, new C([]));
-console.log('BYE');
+console.log('second annotate done');
 
 console.log(S$.annotations(a));
 console.log('' + a);
