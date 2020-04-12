d8bb2ca6e12a48e947c217a7d8f22407d5bb6b16
Blake Loring <blake@parsed.uk>
2020-04-12 18:51:53 +0800

    Remove debugging messages and update Z3

---
+++ tests/strings/yoshiki.js
@@ -0,0 +1,22 @@
+var S$ = require('S$');
+var x = S$.symbol("X", '2');
+
+S$.assume(x.indexOf('=') === -1);
+
+var url = 'https://experiment.com?z=' + x;
+
+console.log(url, url.indexOf('?'));
+
+S$.assume(url.indexOf('?') > 0);
+
+const hashes = url.slice(url.indexOf('?') + 1).split('&');
+const params = {}
+
+hashes.map(hash => {
+	const [key, val] = hash.split('=')
+	params[key] = decodeURIComponent(val)
+});
+
+if(params['z'] === '3') {
+	throw "Hello Z3.";
+}
