ec03edf85f883248612b1d498c6a7d9189d16d6f
Blake Loring <blake@parsed.uk>
2024-07-03 05:48:12 +0800

    Fix warning about Buffer

--- Distributor/src/Log.js
+++ Distributor/src/Log.js
@@ -1,8 +1,8 @@
 /* Copyright (c) Royal Holloway, University of London | Contact Blake Loring (blake@parsed.uk), Duncan Mitchell (Duncan.Mitchell.2015@rhul.ac.uk), or Johannes Kinder (johannes.kinder@rhul.ac.uk) for details or support | LICENSE.md for license details */
 
-const MOVE_LEFT = new Buffer("1b5b3130303044", "hex").toString();
-const MOVE_UP = new Buffer("1b5b3141", "hex").toString();
-const CLEAR_LINE = new Buffer("1b5b304b", "hex").toString();
+const MOVE_LEFT = Buffer.from("1b5b3130303044", "hex").toString();
+const MOVE_UP = Buffer.from("1b5b3141", "hex").toString();
+const CLEAR_LINE = Buffer.from("1b5b304b", "hex").toString();
 
 /**
  * Mock to remove dependency on stringWidth
